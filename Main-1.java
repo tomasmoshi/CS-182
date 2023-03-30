a43eef43c76689920f417e382dfd9b847eefce4f
tomasmoshi <111719125+tomasmoshi@users.noreply.github.com>
2023-03-30 00:00:02 -0700

    Hello World

---
+++ Main-1.java
@@ -0,0 +1,14 @@
+/**
+* This is my first java app.  It prints out Hello World 
+* to the console.
+ * @author Tomas Moshi, tomas.alosh@gmail.com
+ * @version v1.0
+ * @since 8/23/2022 (Tomas Moshi, v1.0)
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Tomas Moshi");
+  }
+}
